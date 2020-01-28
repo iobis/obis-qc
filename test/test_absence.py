24403844075dcb15d119e7592a472f86df7927d3
Pieter Provoost <pieterprovoost@gmail.com>
2020-01-28 15:50:45 +0100

    individualCount

--- test/test_absence.py
+++ test/test_absence.py
@@ -20,6 +20,23 @@ class TestAbsence(unittest.TestCase):
         self.assertFalse(results[4]["absence"])
         self.assertIn("occurrenceStatus", results[4]["invalid"])
 
+    def test_individualcount(self):
+        records = [
+            {"id": 0},
+            {"id": 1, "individualCount": "0" },
+            {"id": 2, "individualCount": "0.0" },
+            {"id": 3, "individualCount": 0 },
+            {"id": 4, "individualCount": 0.0 },
+            {"id": 5, "individualCount": 1 }
+        ]
+        results = absence.check(records)
+        self.assertFalse(results[0]["absence"])
+        self.assertTrue(results[1]["absence"])
+        self.assertTrue(results[2]["absence"])
+        self.assertTrue(results[3]["absence"])
+        self.assertTrue(results[4]["absence"])
+        self.assertFalse(results[5]["absence"])
+
 
 if __name__ == "__main__":
     unittest.main()
