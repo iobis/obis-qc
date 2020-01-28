1cbbce0fc6c3534e5bb244a56fc5548b177da38f
Pieter Provoost <pieterprovoost@gmail.com>
2020-01-28 14:51:27 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="obisqc",
+      version="0.0.1",
+      url="https://github.com/iobis/obis-qc",
+      license="MIT",
+      author="Pieter Provoost",
+      author_email="p.provoost@unesco.org",
+      description="OBIS QC checks",
+      packages=["obisqc"],
+      zip_safe=False)
