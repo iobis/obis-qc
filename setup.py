5ba7578da5a93534e4c82f426dcd391d430fb1a6
Pieter Provoost <pieterprovoost@gmail.com>
2020-06-26 20:48:06 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(name="obisqc",
-      version="0.0.3",
+      version="0.1.0",
+      python_requires='>=3.6',
       url="https://github.com/iobis/obis-qc",
       license="MIT",
       author="Pieter Provoost",
