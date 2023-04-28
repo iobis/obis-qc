c8d1055025751efe2f4036a3f8a43389c2e455e0
Pieter Provoost <pieterprovoost@gmail.com>
2023-04-28 11:40:28 +0200

    updated min year to 1500

--- obisqc/__init__.py
+++ obisqc/__init__.py
@@ -1,6 +1,5 @@
 from obisqc import absence
 from obisqc import fields
-from obisqc import mof_fields
 from obisqc import location
 from obisqc import taxonomy
 from obisqc import time
@@ -8,9 +7,9 @@ from obisqc.model import AphiaCacheInterface, Record
 from typing import List
 
 
-def check(records: List[Record], xylookup: bool=False, cache: AphiaCacheInterface=None):
+def check(records: List[Record], xylookup: bool = False, cache: AphiaCacheInterface = None):
     absence.check(records)
     fields.check(records)
-    time.check(records)
+    time.check(records, min_year=1500)
     taxonomy.check(records, cache)
     location.check(records, xylookup=xylookup)
