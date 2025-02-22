60599091a1daaef2859b0357739a25964a853d0a
Pieter Provoost <pieterprovoost@gmail.com>
2025-02-22 21:54:48 +0100

    test fix

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ import logging
 
 logger = logging.getLogger("isodateparser").setLevel(logging.INFO)
 
-pytest.main(["test/test_taxonomy.py::TestTaxonomy"])
+# pytest.main(["test/test_taxonomy.py::TestTaxonomy"])
+pytest.main()
